90276d2813cfb437e95d365e2a985ce5ed749aaa
Dragana Vranesevic <dvranesevic50@gmail.com>
2026-01-13 20:25:32 +0100

    fix: show name and surname of the message author

--- src/Modules/Tours/Explorer.Tours.API/Dtos/MessageDto.cs
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/MessageDto.cs
@@ -7,4 +7,6 @@ public class MessageDto
     public string Content { get; set; }
     public DateTime Timestamp { get; set; }
     public int AuthorType { get; set; } // 0=Tourist, 1=Author, 2=Admin
+    public string SenderName { get; set; }
+    public string SenderSurname { get; set; }
 }
