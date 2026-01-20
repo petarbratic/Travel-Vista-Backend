82560809e73453ce5a1ccdf1518ba069edabc24c
Petar Bratić <bratic.petar03@gmail.com>
2026-01-20 20:56:31 +0100

    added new entities: achievement and xpEvent, just created repository classes and interfaces

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/XpEvent.cs
@@ -0,0 +1,49 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public enum XpEventType
+    {
+        TourCompleted,
+        ClubJoined,
+        ReviewWritten
+    }
+
+    public class XpEvent : Entity
+    {
+        public long TouristId { get; private set; }
+        public XpEventType Type { get; private set; }
+        public int Amount { get; private set; }
+        public DateTime CreatedAtUtc { get; private set; }
+        public long? SourcEntityId { get; private set; } // da se ne bi desilo dupliranje xp-a
+
+        public XpEvent() { }
+
+        public XpEvent(long touristId, XpEventType type, int amount, long? sourceEntityId = null)
+        {
+            if (touristId == 0) throw new ArgumentException("Invalid TouristId.");
+            if (amount <= 0) throw new ArgumentException("Amount must be > 0.");
+            TouristId = touristId;
+            Type = type;
+            Amount = amount;
+            CreatedAtUtc = DateTime.UtcNow;
+            SourcEntityId = sourceEntityId;
+        }
+        public void Update(long touristId, XpEventType type, int amount, long? sourceEntityId = null)
+        {
+            if (touristId == 0) throw new ArgumentException("Invalid TouristId.");
+            if (amount <= 0) throw new ArgumentException("Amount must be > 0.");
+            TouristId = touristId;
+            Type = type;
+            Amount = amount;
+            SourcEntityId = sourceEntityId;
+        }
+    }
+}
+
+
