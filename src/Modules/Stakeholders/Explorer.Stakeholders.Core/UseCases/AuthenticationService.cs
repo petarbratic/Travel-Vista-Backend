7793afccd6151390c95b8fd58881c8865f52ec48
Marko Velimirovic <94904694+velimirovic@users.noreply.github.com>
2026-01-21 23:43:56 +0100

    Fix WelcomeBonusService in AuthenticationService
    Added welcome bonus service to the authentication process.

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/UseCases/AuthenticationService.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/UseCases/AuthenticationService.cs
@@ -13,11 +13,13 @@ public class AuthenticationService : IAuthenticationService
     private readonly IPersonRepository _personRepository;
     private readonly IWalletRepository _walletRepository;
     private readonly ITouristRepository _touristRepository; 
+    private readonly IWelcomeBonusService _welcomeBonusService;
 
     public AuthenticationService(
         IUserRepository userRepository,
         IPersonRepository personRepository,
         ITokenGenerator tokenGenerator,
+        IWalletRepository walletRepository,
         IWalletRepository walletRepository,
         ITouristRepository touristRepository)
     {
@@ -25,6 +27,7 @@ public class AuthenticationService : IAuthenticationService
         _userRepository = userRepository;
         _personRepository = personRepository;
         _walletRepository = walletRepository;
+        _welcomeBonusService = welcomeBonusService;
         _touristRepository = touristRepository; 
     }
 
@@ -62,8 +65,10 @@ public class AuthenticationService : IAuthenticationService
 
         // 3. Kreiraj Wallet
         var wallet = _walletRepository.Create(new Wallet(person.Id));
-
-        // 4. NOVO: Kreiraj Tourist entitet
+        
+        _welcomeBonusService.CreateWelcomeBonus(person.Id);
+        
+        // 4. Kreiraj Tourist entitet
         var tourist = _touristRepository.Create(new Tourist(person.Id));
 
         return _tokenGenerator.GenerateAccessToken(user, person.Id);
